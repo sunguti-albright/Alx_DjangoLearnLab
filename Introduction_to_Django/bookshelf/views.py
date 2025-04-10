50129edb50dc12cc689a90e03e723be0224fe5c3
sunguti-albright <sungutialbright@gmail.com>
2025-04-10 08:40:40 +0300

    create books view

--- Introduction_to_Django/bookshelf/views.py
+++ Introduction_to_Django/bookshelf/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+def hello_view(request):
+    return HttpResponse("Hello!")
+
+
+def book_list(request):
+    books = Book.objects.all()
+    context = {'book_list':books}
+    return render(request, 'books/book_list.html', context)
