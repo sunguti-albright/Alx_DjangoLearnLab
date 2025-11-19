e3a49df5a78f7e0584f46e9432032f4c2a1af9cd
sunguti-albright <sungutialbright@gmail.com>
2025-11-19 17:56:15 +0300

    changes

--- Introduction_to_Django/bookshelf/models.py
+++ Introduction_to_Django/bookshelf/models.py
@@ -8,3 +8,24 @@ class Book(models.Model):
     
     def __str__(self):
         return self.title
+
+#Create a model representing a company with departments and employees, using ForeignKey relationships
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+     
+    def __str__(self):
+        return self.name
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    company = models.ForeignKey(Company,on_delete=models.CASCADE, related_name='departments')
+    def __str__(self):
+        return self.name
+    
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    on_contract = models.BooleanField()
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='employees')
+    def __str__(self):
+        return self.name
